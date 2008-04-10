9c93bf0ddf80693c2d8d52d88b4624c8fa92315f
Andrew Clunis <andrew@orospakr.ca>
2008-04-10 13:58:48 -0400

    Now has simple setuptools installer.  The installer doesn't install the D-Bus security policy, though.

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
-import distutils.core import setup
+#!/usr/bin/env python
 
-setup(name="Peephole",
-      version="0.2.0",
-      py_modules=['peephole'],
-      scripts=['peepholed'],
-      )
+from setuptools import setup, find_packages
+setup(
+    name = "Peephole",
+    version = "0.2",
+    packages = find_packages(),
+    #    scripts = ['say_hello.py'],
 
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['pyusb>=0.4.1'],
 
+    #     package_data = {
+    #         # If any package contains *.txt or *.rst files, include them:
+        #         '': ['*.txt', '*.rst'],
+    #         # And include any *.msg files found in the 'hello' package, too:
+        #         'hello': ['*.msg'],
+    #     }
+
+    entry_points = {
+        'console_scripts': [
+            'peephole = peephole.peepholed:main'
+            ]
+        },
+
+    # metadata for upload to PyPI
+    author = "Andrew Clunis",
+    author_email = "andrew@orospakr.ca",
+    description = "A simple D-Bus daemon that provides a simple API for accessing attached segmented/low resolution matrix LCD displays",
+    license = "GPLv3",
+    keywords = "lcd dbus driver abstraction",
+    url = "http://www.orospakr.ca/"   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+    )
